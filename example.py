b73cd70f3056c1e3357ce918a9f9846a8c26f156
Diana <diana.lania@interia.pl>
2023-05-08 15:05:07 +0200

    Initial commit

--- example.py
+++ example.py
@@ -1,4 +1,4 @@
-from PyQt6.QtWidgets import QApplication, QVBoxLayout, QLabel, QWidget, QGridLayout, \
+from PyQt6.QtWidgets import QApplication, QLabel, QWidget, QGridLayout, \
      QLineEdit, QPushButton
 
 import sys
